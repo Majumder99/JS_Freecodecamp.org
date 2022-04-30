fc1b46a61048a829b8acd76566f3ba56fc6f7c85
Sourav Majumder <96306119+Majumder99@users.noreply.github.com>
2022-04-30 13:29:54 +0600

    new

---
+++ dsSlice.js
@@ -0,0 +1,6 @@
+// slice will copies the value of array into another array
+// Without changing the main array
+
+const arr = [3, 4, 5, 6, 7, 8, 9, 10];
+const newArr = arr.slice(3, 6); // newArr will have elements before index 6
+console.log(newArr);
--- dsSplice.js
+++ dsSplice.js
@@ -1,7 +1,13 @@
 const array = [1, 2, 3, 4, 5, 6, 6, 7, 8];
 
-const newArray = array.splice(3, 2); // first parameter is for the index and second parameter is for the number of items going to delete
+// const newArray = array.splice(3, 2); // first parameter is for the index and second parameter is for the number of items going to delete
 //It will also return the removed items as an array
 
+// console.log(array);
+// console.log(newArray);
+
+// Three parameters (2,3,5,6) index from 2, 3 elements will be deleted and 5 and 6 will be added on index 2
+
+const latestArray = array.splice(3, 2, 100, 200);
+
 console.log(array);
-console.log(newArray);
