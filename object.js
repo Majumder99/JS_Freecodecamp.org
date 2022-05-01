72bec04c890c2491382ec80f89d3fdfa3eb2c385
Sourav Majumder <96306119+Majumder99@users.noreply.github.com>
2022-05-01 12:19:31 +0600

    new

---
+++ object.js
@@ -0,0 +1,35 @@
+// const Person = {
+//   food: "banana",
+//   drink: "wate",
+// };
+
+// console.log(Person["food"]);
+// console.log(Person.drink);
+
+//For...in
+
+const users = {
+  Alan: {
+    online: false,
+  },
+  Jeff: {
+    online: true,
+  },
+  Sarah: {
+    online: false,
+  },
+};
+
+function countOnline(usersObj) {
+  // Only change code below this line
+  let count = 0;
+  for (let user in usersObj) {
+    if (usersObj[user].online === true) {
+      count++;
+    }
+  }
+  return count;
+  // Only change code above this line
+}
+
+console.log(countOnline(users));
